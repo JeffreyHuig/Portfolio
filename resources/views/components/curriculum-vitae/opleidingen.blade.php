6b65d659fb5cbd294918734fc8bba4d8e804e167
JeffreyHuig <jaj.huig@hotmail.com>
2025-01-13 14:38:33 +0100

    Added Training To CV 13-01-2024 14:38

--- resources/views/components/curriculum-vitae/opleidingen.blade.php
+++ resources/views/components/curriculum-vitae/opleidingen.blade.php
@@ -2,6 +2,12 @@
 <div class="grid grid-cols-9 gap-x-0 w-2/3 my-2">
     <div class="grid grid-cols-9 col-span-9 gap-y-4">
         <div class="grid col-span-2 col-end-3 col-start-1">
+            <p>2024</p>
+        </div>
+        <div class="grid col-span-6 col-start-3">
+            <p class="font-bold">Training ISO Veiligheid</p>
+        </div>
+        <div class="grid col-span-2 col-end-3 col-start-1">
             <p>2019 - 2022</p>
         </div>
         <div class="grid col-span-6 col-start-3">
