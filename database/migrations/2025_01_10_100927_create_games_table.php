402b2df992cfb9d2c707dea404acd2fa37130037
JeffreyHuig <jaj.huig@hotmail.com>
2025-01-15 14:22:40 +0100

    Added Description And Release Date To Database 15-01-2025 14:22

--- database/migrations/2025_01_10_100927_create_games_table.php
+++ database/migrations/2025_01_10_100927_create_games_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->date('release_date');
+            $table->string('description')->nullable();
+            $table->string('game_link')->nullable();
             $table->timestamps();
         });
 
