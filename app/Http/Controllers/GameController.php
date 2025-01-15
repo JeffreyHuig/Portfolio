f1426308530581f14c625ec370294737c4bbe2a1
JeffreyHuig <jaj.huig@hotmail.com>
2025-01-15 14:24:12 +0100

    Filled Page With Games Using Grid 15-01-2025 14:24

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Game;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class GameController extends Controller
 {
     public function index(Game $game)
     {
-        $games = Game::all();
+        $games = DB::table('games')->simplePaginate(12);
         return view('games.index', [
-            'games' => $games
+            'games' => $games,
         ]);
     }
 }
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -6,9 +6,32 @@
     </x-slot>
 
     @foreach ($games as $game)
-        <div class="flex-col border-2 border-gray-900/10 rounded-lg bg-white py-6 px-6 w-3/5 flex mb-8 mt-4 mx-auto">
-            {{$game->name}}<br>
-            {{$game->release_date}}
+        <div class="flex-col border-2 border-gray-900/10 rounded-lg bg-white py-4 px-4 w-3/5 flex mb-8 mt-4 mx-auto shadow-md shadow-blue-800">
+            <div class="grid grid-cols-12 grid-rows-4">
+                <div class="grid grid-cols-1 col-span-4 grid-rows-1 row-span-4 self-center">
+                    <img src="{{asset('images/games').'/'.$game->name.'.png'}}" alt="{{$game->name}}">
+                </div>
+                <div class="grid grid-cols-1 col-span-7 col-start-6 grid-rows-1 row-span-1 row-start-1">
+                    @php
+                        $pattern = "/_/";
+                        preg_match_all($pattern, $game->name);
+                        $replacedGameName = preg_replace($pattern, ' ', $game->name);
+                    @endphp
+                    <p class="font-bold text-xl text-right">
+                        {{$replacedGameName}}<br>
+                    </p>
+                </div>
+                <div class="grid grid-cols-1 col-span-8 col-start-10 grid-rows-1 row-span-1 row-start-2">
+                    <p class="text-center">
+                        {{$game->release_date}}
+                    </p>
+                </div>
+                <div class="grid grid-cols-1 col-span-4 col-start-10 grid-rows-1 row-span-1 row-start-4">
+                    <a href="#">
+                        <p class="font-bold text-right">Meer Info ---></p>
+                    </a>
+                </div>
+            </div>
         </div>
     @endforeach
 </x-app-layout>
